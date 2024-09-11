07295c1b912740905a9a974b29f085c077ece8f8
MD. Ahasanul Islam Nafim <nafim191119@gmail.com>
2024-09-12 04:50:21 +0600

    team added

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+import TeamCard from "./TeamCard";
 
 const Team = () => {
+    const [team, setTeam] = useState([]);
+
+    useEffect(() => {
+        fetch("../../../public/team.json")
+            .then((data) => data.json())
+            .then((res) => setTeam(res));
+    }, []);
+
     return (
         <div>
-            
+            <div className="bg-white px-4 py-12 sm:py-16">
+                <div className="text-center mb-16">
+                    <h2 className="text-4xl font-bold text-gray-900 sm:text-4xl uppercase tracking-tight pb-3">Our Success Team</h2>
+                    <p className="text-xl text-gray-600 text-semibold">
+                        Our team of experts combines experience and creativity. We're here to deliver solutions that exceed expectations.
+                    </p>
+                </div>
+                <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-6 gap-4">
+                    {team.map((person) => (
+                        <TeamCard key={person.name} item={person} />
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/pages/Team/TeamCard.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const TeamCard = ({ item }) => {
+    const { imageUrl, name, role } = item;
+
+    return (
+        <div className="mx-auto">
+            <div className="flex flex-col items-center text-center p-6 bg-gray-50 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+                <img alt="" src={imageUrl} className="h-28 w-28 rounded-full mb-4" />
+                <div>
+                    <h3 className="text-xl font-bold text-gray-900">
+                        <Link>{name}</Link>
+                    </h3>
+                    <p className="text-sm font-semibold text-indigo-600">{role}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TeamCard;
